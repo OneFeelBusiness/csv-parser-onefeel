d9d7c7f58d51fded3bee346576baa55cf9d06d2f
Nikita Aseev <aseevni9@gmail.com>
2023-12-26 21:20:33 +0200

    fix2

--- parser.js
+++ parser.js
@@ -1,46 +1,70 @@
-import csv from "csvtojson"
+// import csv from "csvtojson"
 
-let json
-csv()
-  .fromFile("/Users/nikita/Documents/developing_projects/onefeel/sextoywholesale-products.csv")
-  .then(function (jsonArrayObj) { //when parse finished, result will be emitted here.
-    json = jsonArrayObj.map((item, i) => ({
-      "Handle": item.upc,
-      "Title": item.title,
-      "Body (HTML)": item.description,
-      "Vendor": item.brand,
-      "Product Category": "",
-      "Type": item.category,
-      "Tags": "",
-      "Published": "FALSE",
-      "Variant Grams": item.weight ? parseInt(item.weight) * 453.592 : "",
-      "Variant Fulfillment Service": "manual",
-      "Variant Price": parseInt(item.wholesale_price) * 2,
-      "Variant Compare At Price": parseInt(item.wholesale_price) * 4,
-      "Variant Requires Shipping": "TRUE",
-      "Variant Taxable": "TRUE",
-      "Image Src": item.image_url,
-      "Variant Weight Unit": "g",
-      "Status": "draft"
-    }))
 
-    let csvContent = jsonToCsv(json)
+const CSVToJSON = (data, delimiter = ',') => {
+  const titles = data.slice(0, data.indexOf('\n')).split(delimiter);
+  return data
+    .slice(data.indexOf('\n') + 1)
+    .split('\n')
+    .map(v => {
+      const values = v.split(delimiter);
+      return titles.reduce(
+        (obj, title, index) => ((obj[title] = values[index]), obj),
+        {}
+      );
+    });
+};
 
-    window.URL = window.webkitURL || window.URL;
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
 
-    var contentType = 'text/csv';
+function handleSubmit(event) {
 
-    var csvFile = new Blob([CSV], { type: contentType });
+  console.log(event)
 
-    var a = document.createElement('a');
-    a.download = 'my.csv';
-    a.href = window.URL.createObjectURL(csvFile);
-    a.textContent = 'Download CSV';
+}
 
-    a.dataset.downloadurl = [contentType, a.download, a.href].join(':');
+// let json
+// csv()
+//   .fromFile("/Users/nikita/Documents/developing_projects/onefeel/sextoywholesale-products.csv")
+//   .then(function (jsonArrayObj) { //when parse finished, result will be emitted here.
+//     json = jsonArrayObj.map((item, i) => ({
+//       "Handle": item.upc,
+//       "Title": item.title,
+//       "Body (HTML)": item.description,
+//       "Vendor": item.brand,
+//       "Product Category": "",
+//       "Type": item.category,
+//       "Tags": "",
+//       "Published": "FALSE",
+//       "Variant Grams": item.weight ? parseInt(item.weight) * 453.592 : "",
+//       "Variant Fulfillment Service": "manual",
+//       "Variant Price": parseInt(item.wholesale_price) * 2,
+//       "Variant Compare At Price": parseInt(item.wholesale_price) * 4,
+//       "Variant Requires Shipping": "TRUE",
+//       "Variant Taxable": "TRUE",
+//       "Image Src": item.image_url,
+//       "Variant Weight Unit": "g",
+//       "Status": "draft"
+//     }))
 
-    document.body.appendChild(a);
-  })
+//     let csvContent = jsonToCsv(json)
+
+//     window.URL = window.webkitURL || window.URL;
+
+//     var contentType = 'text/csv';
+
+//     var csvFile = new Blob([CSV], { type: contentType });
+
+//     var a = document.createElement('a');
+//     a.download = 'my.csv';
+//     a.href = window.URL.createObjectURL(csvFile);
+//     a.textContent = 'Download CSV';
+
+//     a.dataset.downloadurl = [contentType, a.download, a.href].join(':');
+
+//     document.body.appendChild(a);
+//   })
 
 function jsonToCsv(items) {
   const header = Object.keys(items[0]);
