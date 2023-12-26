ac39ecb6920b384928f87f764f96595fbe2bb5be
Nikita Aseev <aseevni9@gmail.com>
2023-12-26 22:24:23 +0200

    fix15

--- parser.js
+++ parser.js
@@ -25,30 +25,30 @@ function handleSubmit(event) {
 
   reader.onload = function () {
     console.log(reader.result)
-    // let jsonData = CSVToJSON(reader.result)
-    // let jsonOutput = jsonData.map((item, i) => {
-    //   return {
-    //     "Handle": item.upc,
-    //     "Title": item.title,
-    //     "Body (HTML)": item.description,
-    //     "Vendor": item.brand,
-    //     "Product Category": "",
-    //     "Type": item.category,
-    //     "Tags": "",
-    //     "Published": "FALSE",
-    //     "Variant Grams": item.weight ? parseFloat(item.weight) * 453.592 : "",
-    //     "Variant Fulfillment Service": "manual",
-    //     "Variant Price": parseFloat(item.wholesale_price) * 2,
-    //     "Variant Compare At Price": parseFloat(item.wholesale_price) * 4,
-    //     "Variant Requires Shipping": "TRUE",
-    //     "Variant Taxable": "TRUE",
-    //     "Image Src": item.image_url,
-    //     "Variant Weight Unit": "g",
-    //     "Status": "draft"
-    //   }
-    // })
-    // console.log(jsonOutput);
-    // console.log(jsonData);
+    let jsonData = CSVToJSON(reader.result, ",")
+    let jsonOutput = jsonData.map((item, i) => {
+      return {
+        "Handle": item.upc,
+        "Title": item.title,
+        "Body (HTML)": item.description,
+        "Vendor": item.brand,
+        "Product Category": "",
+        "Type": item.category,
+        "Tags": "",
+        "Published": "FALSE",
+        "Variant Grams": item.weight ? parseFloat(item.weight) * 453.592 : "",
+        "Variant Fulfillment Service": "manual",
+        "Variant Price": parseFloat(item.wholesale_price) * 2,
+        "Variant Compare At Price": parseFloat(item.wholesale_price) * 4,
+        "Variant Requires Shipping": "TRUE",
+        "Variant Taxable": "TRUE",
+        "Image Src": item.image_url,
+        "Variant Weight Unit": "g",
+        "Status": "draft"
+      }
+    })
+    console.log(jsonOutput);
+    console.log(jsonData);
   };
 }
 
